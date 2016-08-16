83d3e02fd90fe29251083447f13ef92acfcfa1a1
nunojusto <nuno@justoweb.com>
2016-08-16 22:10:14 +0100

    Update launch.sh
    Just a correction of port in use test algoritm.
    This way we will not have problems when using port X and having some other service using zyX or any *X port

--- utils/launch.sh
+++ utils/launch.sh
@@ -69,7 +69,7 @@ done
 which netstat >/dev/null 2>&1 \
     || die "Must have netstat installed"
 
-netstat -ltn | grep -qs "${PORT} .*LISTEN" \
+netstat -ltn | grep -qs ":${PORT} .*LISTEN" \
     && die "Port ${PORT} in use. Try --listen PORT"
 
 trap "cleanup" TERM QUIT INT EXIT
