8d710e8b188a57f1acf9394b6bfbe7c150a7a795
Pierre Ossman <ossman@cendio.se>
2016-11-08 14:54:48 +0100

    Blocking transition screen when connecting/disconnecting

--- app/locale/locale-en-GB.js
+++ app/locale/locale-en-GB.js
@@ -5,7 +5,7 @@ Language = {
     "Disconnect timeout": "Disconnect timeout",
     "Password is required": "Password is required",
     "Forcing clipping mode since scrollbars aren't supported by IE in fullscreen": "Forcing clipping mode since scrollbars aren't supported by IE in fullscreen",
-    "Connecting": "Connecting",
-    "Disconnecting": "Disconnecting",
+    "Connecting...": "Connecting...",
+    "Disconnecting...": "Disconnecting...",
     "Disconnected": "Disconnected",
 };
