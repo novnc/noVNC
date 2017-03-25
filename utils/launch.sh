69259cef8a263c6120a0845dc0efd98832d65026
neilpang <github@byneil.com>
2017-03-25 18:49:07 +0800

    all user to set env "SSLONLY" to any value, then `--ssl-only` will work.

--- utils/launch.sh
+++ utils/launch.sh
@@ -140,7 +140,7 @@ fi
 
 echo "Starting webserver and WebSockets proxy on port ${PORT}"
 #${HERE}/websockify --web ${WEB} ${CERT:+--cert ${CERT}} ${PORT} ${VNC_DEST} &
-${WEBSOCKIFY} ${SSLONLY} --web ${WEB} ${CERT:+--cert ${CERT}} ${PORT} ${VNC_DEST} &
+${WEBSOCKIFY} ${SSLONLY:+--ssl-only} --web ${WEB} ${CERT:+--cert ${CERT}} ${PORT} ${VNC_DEST} &
 proxy_pid="$!"
 sleep 1
 if ! ps -p ${proxy_pid} >/dev/null; then
