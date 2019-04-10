c33a668f0747b872847326df795b41e2963fad91
Pawel Raczyk <pawel.raczyk@vectorsynergy.com>
2019-04-10 10:27:08 +0200

    removing unnecessary tests

--- tests/test.localization.js
+++ tests/test.localization.js
@@ -33,40 +33,5 @@ describe('Localization', function () {
         it('should use English by default', function () {
             expect(l10n.language).to.equal('en');
         });
-        it('should use English if no user language matches', function () {
-            window.navigator.languages = ["nl", "de"];
-            l10n.setup(["es", "fr"]);
-            expect(l10n.language).to.equal('en');
-        });
-        it('should use the most preferred user language', function () {
-            window.navigator.languages = ["nl", "de", "fr"];
-            l10n.setup(["es", "fr", "de"]);
-            expect(l10n.language).to.equal('de');
-        });
-        it('should prefer sub-languages languages', function () {
-            window.navigator.languages = ["pt-BR"];
-            l10n.setup(["pt", "pt-BR"]);
-            expect(l10n.language).to.equal('pt-BR');
-        });
-        it('should fall back to language "parents"', function () {
-            window.navigator.languages = ["pt-BR"];
-            l10n.setup(["fr", "pt", "de"]);
-            expect(l10n.language).to.equal('pt');
-        });
-        it('should not use specific language when user asks for a generic language', function () {
-            window.navigator.languages = ["pt", "de"];
-            l10n.setup(["fr", "pt-BR", "de"]);
-            expect(l10n.language).to.equal('de');
-        });
-        it('should handle underscore as a separator', function () {
-            window.navigator.languages = ["pt-BR"];
-            l10n.setup(["pt_BR"]);
-            expect(l10n.language).to.equal('pt_BR');
-        });
-        it('should handle difference in case', function () {
-            window.navigator.languages = ["pt-br"];
-            l10n.setup(["pt-BR"]);
-            expect(l10n.language).to.equal('pt-BR');
-        });
     });
 });
