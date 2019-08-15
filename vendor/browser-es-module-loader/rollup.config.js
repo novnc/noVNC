604edf07d23345c8adf96527995323473a3baace
Pierre Ossman <ossman@cendio.se>
2019-08-15 15:38:21 +0200

    Upgrade to latest rollup

--- vendor/browser-es-module-loader/rollup.config.js
+++ vendor/browser-es-module-loader/rollup.config.js
@@ -1,16 +1,15 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
-  entry: 'src/browser-es-module-loader.js',
-  dest: 'dist/browser-es-module-loader.js',
-  format: 'umd',
-  moduleName: 'BrowserESModuleLoader',
-  sourceMap: true,
+  input: 'src/browser-es-module-loader.js',
+  output: {
+    file: 'dist/browser-es-module-loader.js',
+    format: 'umd',
+    name: 'BrowserESModuleLoader',
+    sourcemap: true,
+  },
 
   plugins: [
     nodeResolve(),
   ],
-
-  // skip rollup warnings (specifically the eval warning)
-  onwarn: function() {}
 };
