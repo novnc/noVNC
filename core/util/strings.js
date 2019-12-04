80b078c469465f1ec45a5251a40eee51f13c5a1d
Samuel Mannehed <samuel@cendio.se>
2019-12-04 10:13:25 +0100

    Add encodeUTF8 function to core/util/strings.js

--- core/util/strings.js
+++ core/util/strings.js
@@ -1,14 +1,17 @@
 /*
  * noVNC: HTML5 VNC client
- * Copyright (C) 2018 The noVNC Authors
+ * Copyright (C) 2019 The noVNC Authors
  * Licensed under MPL 2.0 (see LICENSE.txt)
  *
  * See README.md for usage and integration instructions.
  */
 
-/*
- * Decode from UTF-8
- */
+// Decode from UTF-8
 export function decodeUTF8(utf8string) {
     return decodeURIComponent(escape(utf8string));
+}
+
+// Encode to UTF-8
+export function encodeUTF8(DOMString) {
+    return unescape(encodeURIComponent(DOMString));
 }
