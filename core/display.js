9f4a734a4eed07482686d5b95a1b91ac10e04c21
Alt, Michael <e10013@extern.inovex.de>
2020-01-15 16:30:00 +0100

    added debugging for scan render que

--- core/display.js
+++ core/display.js
@@ -6,6 +6,7 @@
  * See README.md for usage and integration instructions.
  */
 
+const urlParams = new URLSearchParams(window.location.search);
 
 import * as Log from './util/logging.js';
 import Base64 from "./base64.js";
@@ -789,21 +790,39 @@ export default class Display {
                     this.flip(true);
                     break;
                 case 'copy':
+                    if (urlParams.get('scanRender').contains('copy')) {
+                        console.debug("_scan_renderQ: copy", a);
+                    }
                     this.copyImage(a.old_x, a.old_y, a.x, a.y, a.width, a.height, true);
                     break;
                 case 'fill':
+                    if (urlParams.get('scanRender').contains('fill')) {
+                        console.debug("_scan_renderQ: fill", a);
+                    }
                     this.fillRect(a.x, a.y, a.width, a.height, a.color, true);
                     break;
                 case 'blit':
+                    if (urlParams.get('scanRender').contains('blit')) {
+                        console.debug("_scan_renderQ: blit", a);
+                    }
                     this.blitImage(a.x, a.y, a.width, a.height, a.data, 0, true);
                     break;
                 case 'blitRgb':
+                    if (urlParams.get('scanRender').contains('blitRgb')) {
+                        console.debug("_scan_renderQ: blitRgb", a);
+                    }
                     this.blitRgbImage(a.x, a.y, a.width, a.height, a.data, 0, true);
                     break;
                 case 'blitRgbx':
+                    if (urlParams.get('scanRender').contains('blitRgbx')) {
+                        console.debug("_scan_renderQ: blitRgbx", a);
+                    }
                     this.blitRgbxImage(a.x, a.y, a.width, a.height, a.data, 0, true);
                     break;
                 case 'img':
+                    if (urlParams.get('scanRender').contains('img')) {
+                        console.debug("_scan_renderQ: img", a);
+                    }
                     if (a.img.complete) {
                         this.drawImage(a.img, a.x, a.y);
                     } else {
