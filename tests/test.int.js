9575ded8da83b6d8774b36316c388279fa0512cc
Niko Lehto <nikle@cendio.se>
2020-01-28 17:00:04 +0100

    Add util for unsigned and signed int. conversion
    Will be used in later commit in extended clipboard handling.

---
+++ tests/test.int.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+const expect = chai.expect;
+
+import { toUnsigned32bit, toSigned32bit } from '../core/util/int.js';
+
+describe('Integer casting', function () {
+    it('should cast unsigned to signed', function () {
+        let expected = 4294967286;
+        expect(toUnsigned32bit(-10)).to.equal(expected);
+    });
+
+    it('should cast signed to unsigned', function () {
+        let expected = -10;
+        expect(toSigned32bit(4294967286)).to.equal(expected);
+    });
+});
