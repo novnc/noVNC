34f52a8f41ec677463dc331ae31b89db32883817
Pierre Ossman <ossman@cendio.se>
2020-06-07 13:36:15 +0200

    Fix bad BasicCompression check in Tight decoder

--- core/decoders/tight.js
+++ core/decoders/tight.js
@@ -56,7 +56,7 @@ export default class TightDecoder {
         } else if (this._ctl === 0x0A) {
             ret = this._pngRect(x, y, width, height,
                                 sock, display, depth);
-        } else if ((this._ctl & 0x80) == 0) {
+        } else if ((this._ctl & 0x08) == 0) {
             ret = this._basicRect(this._ctl, x, y, width, height,
                                   sock, display, depth);
         } else {
