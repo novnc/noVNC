643442feac2bf95f526738eab5b03a267d38fb85
Pierre Ossman <ossman@cendio.se>
2020-06-23 13:54:46 +0200

    Remove default Karma options
    No need to mention things where we already use the default value.

--- karma.conf.js
+++ karma.conf.js
@@ -92,14 +92,6 @@ module.exports = (config) => {
     reporters: ['mocha'],
 
 
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
