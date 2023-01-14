b7ef484fbf08a30d32cb7ee36cf347a5fc614924
Vitor Rodrigues <vsilvar@users.noreply.github.com>
2023-01-14 14:48:51 +0100

    Prefix websocket path with server path
    Fixes #1737 and it should work for all of the different scenarios where the relative assets also work.
    This includes, for example:
       - /
       - /vnc.html
       - /vnc/
       - /vnc/vnc.html
    
    It won't work for:
     - /vnc (but the assets don't work either, so it's consistent)
     - or /vnc.html/ (but this results in a 404 by default anyway)

--- app/ui.js
+++ app/ui.js
@@ -180,7 +180,7 @@ const UI = {
         UI.initSetting('shared', true);
         UI.initSetting('view_only', false);
         UI.initSetting('show_dot', false);
-        UI.initSetting('path', 'websockify');
+        UI.initSetting('path', window.location.pathname.substr(1) + '/../websockify');
         UI.initSetting('repeaterID', '');
         UI.initSetting('reconnect', false);
         UI.initSetting('reconnect_delay', 5000);
