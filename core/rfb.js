37320aa55a0408bb5a78a01e3c0604e28c7ee0ab
Philipp <pjanos@gmx.at>
2023-08-28 12:01:43 +0200

    Continue with ClientInitialisation when SecurityType is None

--- core/rfb.js
+++ core/rfb.js
@@ -1925,7 +1925,7 @@ export default class RFB extends EventTargetMixin {
     _negotiateAuthentication() {
         switch (this._rfbAuthScheme) {
             case securityTypeNone:
-                this._rfbInitState = 'SecurityResult';
+                this._rfbInitState = 'ClientInitialisation';
                 return true;
 
             case securityTypeXVP:
