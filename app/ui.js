6885a428846ac70b87f184f95befaa01f2ad4b84
highxshell <tendernessseizures@gmail.com>
2025-09-18 11:07:48 +0500

    hide transition text, always reconnect

--- app/ui.js
+++ app/ui.js
@@ -187,7 +187,7 @@ const UI = {
         UI.initSetting('show_dot', false);
         UI.initSetting('path', 'websockify');
         UI.initSetting('repeaterID', '');
-        UI.initSetting('reconnect', false);
+        UI.initSetting('reconnect', true);
         UI.initSetting('reconnect_delay', 5000);
     },
     // Adds a link to the label elements on the corresponding input elements
